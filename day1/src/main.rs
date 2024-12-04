399dc6265b0382ebd2b59a2f0215d9ba5712e533
Séamus O'Sullivan <m0ose01@proton.me>
2024-12-04 16:37:17 +0000

    Make more concise.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,18 +4,19 @@ fn main() {
     let stdin = io::stdin();
     let handle = stdin.lock();
 
-    let (mut list_a, mut list_b) = parse_input(handle);
-    list_a.sort(); list_b.sort();
-    let list_a = list_a; let list_b = list_b;
+    let lists: Vec<Vec<i32>> = parse_input(handle)
+        .into_iter()
+        .map(|mut v| {v.sort(); v})
+        .collect();
 
-    let list_length = list_a.len();
+    let list_length = lists[0].len();
 
-    assert!(list_length == list_b.len());
+    assert!(list_length == lists[1].len());
 
     let mut diff: Vec<u32> = Vec::with_capacity(list_length);
 
-    for ii in 0..list_a.len() {
-        diff.push(list_a[ii].abs_diff(list_b[ii]));
+    for ii in 0..lists[0].len() {
+        diff.push(lists[0][ii].abs_diff(lists[1][ii]));
     }
 
     let sum: u32 = diff.into_iter().sum();
@@ -23,10 +24,11 @@ fn main() {
     println!("{sum}");
 }
 
-fn parse_input(input: impl BufRead) -> (Vec<i32>, Vec<i32>) {
-    input.lines()
+fn parse_input(input: impl BufRead) -> [Vec<i32>; 2] {
+    let t = input.lines()
         .map(|line| parse_line(&line.expect("Problem parsing string.")))
-        .unzip()
+        .unzip();
+    [t.0, t.1]
 }
 
 fn parse_line(line: &str) -> (i32, i32) {
