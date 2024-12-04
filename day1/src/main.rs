949807e9f5ad4180c6eff0cdd9d489346344036c
Séamus O'Sullivan <m0ose01@proton.me>
2024-12-04 11:39:38 +0000

    Change to functional approach for day1.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,25 +4,15 @@ fn main() {
     let stdin = io::stdin();
     let handle = stdin.lock();
 
-    let mut list_a: Vec<i32> = vec![];
-    let mut list_b: Vec<i32> = vec![];
-
-    for line in handle.lines() {
-        split_lists(
-            &line.expect("Problem parsing string."),
-            &mut list_a,
-            &mut list_b
-        );
-    }
+    let (mut list_a, mut list_b) = parse_input(handle);
+    list_a.sort(); list_b.sort();
+    let list_a = list_a; let list_b = list_b;
 
     let list_length = list_a.len();
 
     assert!(list_length == list_b.len());
 
     let mut diff: Vec<u32> = Vec::with_capacity(list_length);
-
-    list_a.sort();
-    list_b.sort();
 
     for ii in 0..list_a.len() {
         diff.push(list_a[ii].abs_diff(list_b[ii]));
@@ -33,10 +23,14 @@ fn main() {
     println!("{sum}");
 }
 
-fn split_lists(line: &str, list_a: &mut Vec<i32>, list_b: &mut Vec<i32>) {
-    let inputs: Vec<i32> = line.split_whitespace()
-        .map(|x| x.parse().expect("Not an integer."))
-        .collect();
-    list_a.push(inputs[0]);
-    list_b.push(inputs[1]);
+fn parse_input(input: impl BufRead) -> (Vec<i32>, Vec<i32>) {
+    input.lines()
+        .map(|line| parse_line(&line.expect("Problem parsing string.")))
+        .unzip()
+}
+
+fn parse_line(line: &str) -> (i32, i32) {
+    let nums: Vec<i32> = line.split_whitespace()
+    .map(|c| c.parse::<i32>().expect("Not an integer")).collect();
+    (nums[0], nums[1])
 }
