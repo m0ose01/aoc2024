d567b33ea07bbb2262490cbe44bc43c04a603aa2
Séamus O'Sullivan <m0ose01@proton.me>
2024-12-04 18:17:43 +0000

    Parse input of day2.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,20 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin().lock();
+    let input = parse_input(stdin);
+    println!("{:?}", input);
+}
+
+fn parse_input(input: impl BufRead) -> Vec<Vec<i32>> {
+    input.lines()
+    .map(|line| parse_report(&line.expect("Problem parsing input.")))
+    .collect()
+}
+
+fn parse_report(input: &str) -> Vec<i32> {
+    input
+        .split_whitespace()
+        .map(|x| x.parse().expect("Not an integer"))
+        .collect()
 }
